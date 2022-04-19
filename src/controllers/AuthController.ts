17fab70dd72b616bca150f6f8f17e7b1d804ea5c
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-04-19 20:40:00 -0300

    Inclusão de mensagem de erro na API de Autenticação

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -9,12 +9,12 @@ class AuthController {
 
     const user = await User.findOne({ email }).select('+password')
     if (!user) {
-      return res.sendStatus(401)
+      return res.sendStatus(401).json({ message: 'e-mail or password invalid' })
     }
 
     const isValidPassword = await bcryptjs.compare(password, user.password)
     if (!isValidPassword) {
-      return res.sendStatus(401)
+      return res.sendStatus(401).json({ message: 'e-mail or password invalid' })
     }
 
     const token = jwt.sign({ id: user._id }, process.env.SECRET_AUTH, { expiresIn: '1d' })
